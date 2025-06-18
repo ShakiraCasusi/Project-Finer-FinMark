1f6cbb5c05fd7a225923c0d54c264fcd5e7e717c
Shakira Angela Casusi <casusishakira28@gmail.com>
2025-06-18 19:19:02 +0800

    feat: added JWT middleware and protected route endpoint

--- services/auth-service/src/auth.routes.js
+++ services/auth-service/src/auth.routes.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+// Controllers
 const { register, login } = require('./auth.controller');
+
+// Validators & Middleware
 const { registerRules } = require('./validators/auth.validator');
 const validate = require('./middlewares/validate');
+const requireAuth = require('./middlewares/auth.middleware');
 
+// Routes
 router.post('/register', registerRules, validate, register);
 router.post('/login', login);
+
+// Protected Route using JWT
+router.get('/protected', requireAuth, (req, res) => {
+  res.json({
+    message: `Welcome user ${req.user.userId}, you accessed a protected route.`,
+  });
+});
 
 module.exports = router;
 
